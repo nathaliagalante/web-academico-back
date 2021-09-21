d0737931473b913ba623c0c7325a9485233a9b49
Nathalia Galante <nathalia.g.a@hotmail.com>
2021-09-21 06:26:01 -0300

    feat: acgs

---
+++ src/db/acgs.js
@@ -0,0 +1,88 @@
+const acgs = [
+    {
+        key: 1,
+        categoria: 'Assistir à apresentação de TCC/Monografia',
+        horas: '4',
+        detalhes: [
+            {
+                data: '19/07/2021',
+                descricao: 'TCC I - V JORNADA DE PROJETOS CIENTÍFICOS DA FeMASS',
+                horas: '2'
+            },
+            {
+                data: '19/07/2021',
+                descricao: 'TCC I - V JORNADA DE PROJETOS CIENTÍFICOS DA FeMASS',
+                horas: '2'
+            }
+        ],
+        descricao: 'tentando rodar detalhes'
+    },
+    {
+        key: 2,
+        categoria: 'Cursos de aperfeiçoamento',
+        horas: '12',
+        detalhes: [
+            {
+                data: '10/12/2020',
+                descricao: 'CURSO BUILD RESPONSIVE REAL WORLD WEBSITES WITH HTML5',
+                horas: '12'
+            }
+        ]
+    },
+    {
+        key: 3,
+        categoria: 'Cursos de suporte à formação (excel, idiomas, etc)',
+        horas: '4',
+        detalhes: [
+            {
+                data: '04/12/2017',
+                descricao: 'Semana acadêmica',
+                horas: '4'
+            }
+        ]
+    },
+    {
+        key: 4,
+        categoria: 'Participação em congressos, seminários, fóruns, palestras, colóquios e afins como participante-ouvinte ou como participante-organizador',
+        horas: '30',
+        detalhes: [
+            {
+                data: '30/03/2017',
+                descricao: 'PALESTRA DE GESTÃO DE OPERAÇÕES',
+                horas: '2'
+            },
+            {
+                data: '06/04/2017',
+                descricao: 'PALESTRA DE GESTÃO DA QUALIDADE',
+                horas: '2'
+            },
+            {
+                data: '11/05/2017',
+                descricao: 'PALESTRA SOBRE GESTÃO ESTRATÉGICA ORGANIZACIONAL',
+                horas: '2'
+            },
+            {
+                data: '26/05/2017',
+                descricao: 'PROJETO INTEGRADO - METODOLOGIA DE PESQUISA',
+                horas: '2'
+            },
+            {
+                data: '04/12/2017',
+                descricao: 'I Evento Acadêmico do Curso de Sistemas de Informação',
+                horas: '8'
+            },
+            {
+                data: '04/12/2017',
+                descricao: 'Semana acadêmica',
+                horas: '11'
+            },
+            {
+                data: '18/12/2018',
+                descricao: 'PALESTRA" INTELIGÊNCIA COMPUTACIONAL $ HELLO-FRONT END!',
+                horas: '3'
+            },
+        ]
+    }
+]
+
+module.exports = { acgs };
