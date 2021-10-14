097fc8ef324adbfb420b4ec9f31c6f0c45d315b5
Nathalia Galante <nathalia.g.a@hotmail.com>
2021-10-14 10:28:56 -0300

    hotfix: app port

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(routes);
 
-app.listen(3333, () => console.log('running...'));
+app.listen(process.env.PORT || 3333, () => console.log('running...'));
 
 module.exports = app;
